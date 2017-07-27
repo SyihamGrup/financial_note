af7c26fcaba7fdc38ad5338d22a7969f910e118b
Adi Sayoga <adisayoga@gmail.com>
2017-07-27 10:20:03 +0800

    Tambah copyright.

--- lib/widget/month_picker.dart
+++ lib/widget/month_picker.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2017. All rights reserved.
+//
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Proprietary and confidential.
+//
+// Written by:
+//   - Adi Sayoga <adisayoga@gmail.com>
+
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
--- lib/widget/widget.dart
+++ lib/widget/widget.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2017. All rights reserved.
+//
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Proprietary and confidential.
+//
+// Written by:
+//   - Adi Sayoga <adisayoga@gmail.com>
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
