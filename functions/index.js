158ec239ac90d270330c1fdcee564d5a82ad2164
Adi Sayoga <adisayoga@gmail.com>
2017-07-27 16:43:57 +0800

    Tambah firebase functions.

---
+++ functions/index.js
@@ -0,0 +1,26 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// Get opening balance
+exports.getOpeningBalance = functions.https.onRequest((req, res) => {
+  const uid = req.query.uid;
+  const date = req.query.date;
+
+  admin.database().ref('/balances/' + uid).orderByChild('date').endAt(date).once('value', snapshot => {
+    const data = snapshot.val();
+    if (typeof(data) !== 'object') {
+      res.json({balance: 0});
+      return;
+    }
+
+    let total = 0;
+    for (let key in data) {
+      total += data[key].value || 0;
+    }
+    res.json({balance: total});
+  });
+});
